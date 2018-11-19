b167b93d2c5629aef08b5e18735cb2778f3d313d
huyan <huyan@56matrix.com>
2018-11-19 17:04:34 +0800

    Initial commit

---
+++ src/main/java/com/huyidao/hadoopexamples/HadoopExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.huyidao.hadoopexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HadoopExamplesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/huyidao/hadoopexamples/HadoopExamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.huyidao.hadoopexamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HadoopExamplesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
